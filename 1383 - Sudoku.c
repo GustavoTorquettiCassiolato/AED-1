2e40350ea880da2a4a90c8998e6b45eb68d45e26
GustavoTorquettiCassiolato <gamesdouble676@gmail.com>
2025-02-04 14:32:57 -0300

    Update and rename SudokuFinal.c to  1383 - Sudoku.c

--- 1383 - Sudoku.c
+++ 1383 - Sudoku.c
@@ -5,7 +5,7 @@ int main() {
     int numeroDeMatriz;
     scanf("%d", &numeroDeMatriz);
 
-    for (int matrizes = 1; matrizes <= numeroDeMatriz; matrizes++) { //Número de matrizes estabelecido
+    for (int matrizes = 1; matrizes <= numeroDeMatriz; matrizes++) { //NÃºmero de matrizes estabelecido
         int sudoku[9][9] = {0};
 
         //Entrada das linhas do Sudoku
@@ -15,19 +15,19 @@ int main() {
             }
         }
 
-        int validacao = 1; //Matriz é válida
+        int validacao = 1; //Matriz Ã© vÃ¡lida
 
         for (int linhas = 0; linhas < 9; linhas++) {
-            int linha[10] = {0}; //Verifica repetição em linhas
-            int coluna[10] = {0}; //Verifica repetição em colunas
+            int linha[10] = {0}; //Verifica repetiÃ§Ã£o em linhas
+            int coluna[10] = {0}; //Verifica repetiÃ§Ã£o em colunas
 
             for (int col = 0; col < 9; col++) {
-                //Verificação de linha
+                //VerificaÃ§Ã£o de linha
                 int numero = sudoku[linhas][col];
                 if (numero >= 1 && numero <= 9) {
                     linha[numero]++;
                     if (linha[numero] > 1) {
-                        validacao = 0; //Após verificar se o número está entre 1 e 9, afirma em seu vetor sua aparição, e caso haja repetição, a validacao será 0.
+                        validacao = 0; //ApÃ³s verificar se o nÃºmero estÃ¡ entre 1 e 9, afirma em seu vetor sua apariÃ§Ã£o, e caso haja repetiÃ§Ã£o, a validacao serÃ¡ 0.
                         break;
                     }
                 }
@@ -35,7 +35,7 @@ int main() {
                     validacao = 0; //Caso use um numero abaixo de 1 ou acima de 9.
                 }
 
-                //Verificação de coluna
+                //VerificaÃ§Ã£o de coluna
                 numero = sudoku[col][linhas];
                 if (numero >= 1 && numero <= 9) {
                     coluna[numero]++;
@@ -48,14 +48,14 @@ int main() {
                 }
             }
 
-            // Verificação dos blocos 3x3
+            // VerificaÃ§Ã£o dos blocos 3x3
             for (int blocol = 0; blocol < 3; blocol++) { //Linha do bloco
                 for (int blococ = 0; blococ < 3; blococ++) { //Coluna do bloco
                     int tres[10] = {0};
 
                     for (int i = 0; i < 3; i++) {
                         for (int j = 0; j < 3; j++) {
-                            int numero = sudoku[blocol * 3 + i][blococ * 3 + j]; //Verifica cada um elemento dos 9 em cada bloco em busca de repetições.
+                            int numero = sudoku[blocol * 3 + i][blococ * 3 + j]; //Verifica cada um elemento dos 9 em cada bloco em busca de repetiÃ§Ãµes.
 
                             if (numero >= 1 && numero <= 9) {
                                 tres[numero]++;
@@ -63,7 +63,7 @@ int main() {
                                     validacao = 0;
                                 }
                             } else {
-                                validacao = 0; //Mesmo processo, caso o numero esteja acima de 9 ou abaixo de 1, o sudoku será invalidado.
+                                validacao = 0; //Mesmo processo, caso o numero esteja acima de 9 ou abaixo de 1, o sudoku serÃ¡ invalidado.
                             }
                         }
                     }
